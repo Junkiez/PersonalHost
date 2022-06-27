ac0a214045906600370c860cede5b9483a2bb3c4
Kaze <80313506+Junkiez@users.noreply.github.com>
2022-06-27 13:19:48 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/autotls"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/acme/autocert"
+)
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+
+	publicGroup := router.Group("/")
+	{
+		publicGroup.Static("/", "./web/main")
+		//publicGroup.Static("/cv", "./web/cv")
+	}
+
+	m := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("localhost.com"),
+		Cache:      autocert.DirCache("./cert"),
+	}
+
+	log.Fatal(autotls.RunWithManager(router, &m))
+	//router.RunTLS(getPort(), "./cert/server.pem", "./cert/server.key")
+}
+
+func getPort() string {
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = "443"
+	}
+	return ":" + port
+}
+
+/*
+func loadTls() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		middleware := secure.New(secure.Options{
+			SSLRedirect: true,
+			SSLHost:     "localhost:443",
+		})
+		err := middleware.Process(c.Writer, c.Request)
+		if err != nil {
+			return
+		}
+		c.Next()
+	}
+}*/
